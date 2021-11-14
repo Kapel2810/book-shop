022a7c4c09d63101e650f812d2e50b2861c5b339
Katrina Apel <kapel2810@gmail.com>
2021-11-14 05:11:20 +0200

    remove element done

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 
 let booksList = []
+let bookIdCounter = 0
 
 let nameElement = document.getElementById('book-name')
 let authorElement = document.getElementById('book-author')
@@ -9,21 +10,27 @@ let bookContainerElement = document.getElementById('books-container')
 
 const addNewBook = () => {
 
+    //condition when on of the inputs is invalid or empty
     if (nameElement.value === " " || authorElement.value === " ") {
         alert("Name or Author field is empty. Please try try again")
-        return
+        return //stops the condition
     }
 
-
+   //save to obj
     let newBook = {
+        id: bookIdCounter++,
         name: nameElement.value,
         author: authorElement.value,
         price: Number(priceElement.value)
     }
+    //add to arr
     booksList.push(newBook)
-    //console.log(booksList);
+    console.log(booksList);
 
+   //load the table
     loadBooks()
+
+    //clean the input
     nameElement.value = " "
     authorElement.value=" "
     priceElement.value=" "
@@ -37,6 +44,7 @@ const loadBooks = () => {
             <th> Book Name: </th> 
             <th> Book Author: </th> 
             <th> Book Price: </th> 
+            <th style="color:red;"> Delete: </th> 
         </tr>`
 
 
@@ -45,9 +53,20 @@ const loadBooks = () => {
                         <td>${book.name}</td> 
                         <td>${book.author}</td> 
                         <td>${book.price}</td> 
+                        <td>
+                            <button onclick ="handleDeleteBookByID(${book.id})">x</button>
+                        </td>
                     </tr>`
     }
     table += `</table>`
 
     bookContainerElement.innerHTML = table
+}
+
+const handleDeleteBookByID = (id) => {
+    let filteredBookList = booksList.filter(el => el.id !== id)
+    booksList = filteredBookList
+
+    console.log(filteredBookList);
+    loadBooks()
 }
